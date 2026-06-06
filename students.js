2d7a304ab77dbfb9bda3a2a3cc818404481fb83b
OSS Team <osscyberteam@gmail.com>
2026-06-07 01:59:23 +0600

    Update students.js

--- students.js
+++ students.js
@@ -1,6 +1,6 @@
 // স্টুডেন্টদের ডাটাবেজ (আইডি, পাসওয়ার্ড এবং প্রোফাইল ডিটেলস)
 const studentsDatabase = {
-    "1001": {
+    "101": {
         password: "pass1",
         name: "MD. Rafiqul Islam",
         reg: "REG-2026-8840",
@@ -9,7 +9,7 @@ const studentsDatabase = {
         address: "Dhaka, Bangladesh",
         img: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=200" // স্টুডেন্টের ছবির লিংক
     },
-    "1002": {
+    "102": {
         password: "pass2",
         name: "Anika Rahman",
         reg: "REG-2026-8841",
@@ -18,7 +18,7 @@ const studentsDatabase = {
         address: "Chittagong, Bangladesh",
         img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=200"
     },
-    "1003": {
+    "103": {
         password: "pass3",
         name: "Sajid Ahmed",
         reg: "REG-2026-8842",
@@ -27,7 +27,7 @@ const studentsDatabase = {
         address: "Sylhet, Bangladesh",
         img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=200"
     },
-        "1004": {
+        "104": {
         password: "pass4",
         name: "Anika Rahman",
         reg: "REG-2026-8841",
