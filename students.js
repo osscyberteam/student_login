7a385f83c9b1fad4b42a1b86428d8d41a2d7e9ab
OSS Team <osscyberteam@gmail.com>
2026-07-08 22:23:08 +0600

    Update students.js

--- students.js
+++ students.js
@@ -1,41 +1,5 @@
 // স্টুডেন্টদের ডাটাবেজ উদাহরণ (আইডি, পাসওয়ার্ড এবং প্রোফাইল ডিটেলস)
 const studentsDatabase = {
-    "1001": {
-        password: "pass1",
-        name: "MD. Rafiqul Islam",
-        reg: "REG-2026-8840",
-        mobile: "01700000001",
-        email: "rafiq@example.com",
-        address: "Dhaka, Bangladesh",
-        img: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=200" // ডেমো ছবি লিংক
-    },
-    "1002": {
-        password: "pass2",
-        name: "Anika Rahman",
-        reg: "REG-2026-8841",
-        mobile: "01800000002",
-        email: "anika@example.com",
-        address: "Chittagong, Bangladesh",
-        img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=200"
-    },
-    "1003": {
-        password: "pass3",
-        name: "Sajid Ahmed",
-        reg: "REG-2026-8842",
-        mobile: "01900000003",
-        email: "sajid@example.com",
-        address: "Sylhet, Bangladesh",
-        img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=200"
-    },
-    "1004": {
-        password: "pass4",
-        name: "Sajid Ahmed",
-        reg: "REG-2026-8842",
-        mobile: "01900000003",
-        email: "sajid@example.com",
-        address: "Sylhet, Bangladesh",
-        img: "https://lh3.googleusercontent.com/d/1Ys70CAGvuJVWlFcPpZYw2HGyWI_UDGfH"
-    },
     "Admin": {
         password: "Admin0704",
         name: "Sopno Chakrobortty",
@@ -44,5 +8,113 @@ const studentsDatabase = {
         email: "sc.sopno@gmail.com",
         address: "Jhenaidah, Bangladesh",
         img: "https://lh3.googleusercontent.com/d/1HmHHU7RWXmSpyA_DWl4sf258s4k13y4w"
-    }
+    },
+    "AC1012": {
+    password: "1012",
+    name: "Shaju Ahomed",
+    reg: "NON GOVT ID",
+    mobile: "01309742976",
+    email: "shajuahomed7469@gmail.com",
+    address: "Khagrachhari Sadar",
+    img: "https://lh3.googleusercontent.com/d/1Ys70CAGvuJVWlFcPpZYw2HGyWI_UDGfH"
+},
+"AC1011": {
+    password: "1011",
+    name: "Shoumik kumar biswas",
+    reg: "NON GOVT ID",
+    mobile: "01943736409",
+    email: "Shoumikkumar60@gmail.com",
+    address: "JHENAIDAH",
+    img: "https://lh3.googleusercontent.com/d/1OSgtoOsBTXMrxGfoxXsEJT-xz7xkC5Q-"
+},
+"AC1010": {
+    password: "1010",
+    name: "Md Tawhid Hossen",
+    reg: "NON GOVT ID",
+    mobile: "01821848296",
+    email: "wasimtawhidmejba@gmail.com",
+    address: "Jhenaidah",
+    img: "https://lh3.googleusercontent.com/d/1d_hO1qoDhkTIaIwJ-YS370Db7hShOGti"
+},
+"AC1009": {
+    password: "1009",
+    name: "Dipty Sarker",
+    reg: "NON GOVT ID",
+    mobile: "01304936664",
+    email: "dipti2025.mail@gmail.com",
+    address: "Ghatail Tangail",
+    img: "https://lh3.googleusercontent.com/d/1Uio4dgbOmi4EOFmIq3sqV34b4EaZrdQK"
+},
+"AC1008": {
+    password: "1008",
+    name: "Md Naim khan",
+    reg: "NON GOVT ID",
+    mobile: "01982874928",
+    email: "mdnaimkhan559@gmail.com",
+    address: "ঝিনাইদহ",
+    img: "https://lh3.googleusercontent.com/d/19HnshgHt1ydkfD1gO0HBJFsd1te6a1hU"
+},
+"AC1007": {
+    password: "1007",
+    name: "Jibon Chandro Das",
+    reg: "NON GOVT ID",
+    mobile: "01571323816",
+    email: "dasjibonchandro34@gmail.com",
+    address: "Aditmari, Lalmonirhat.",
+    img: "https://lh3.googleusercontent.com/d/1qiNF2vZcln4ESKaLEdolgiw4b5FCPL7O"
+},
+"AC1005": {
+    password: "1005",
+    name: "Md Rifat hossain Santo",
+    reg: "NON GOVT ID",
+    mobile: "01777919096",
+    email: "mdrifathossainsanto@gmail.com",
+    address: "Pabna Sadar",
+    img: "https://lh3.googleusercontent.com/d/1ifjSMFc7QyZIYGkb1tUhtijr3EYGytFK"
+},
+"AC1004": {
+    password: "1004",
+    name: "Abdulla Al Amin",
+    reg: "NON GOVT ID",
+    mobile: "01841484586",
+    email: "abdullahalamin9909@gmail.com",
+    address: "jhenaida",
+    img: "https://lh3.googleusercontent.com/d/117-ZYvvUUx3HFz5dXMThYAKu09GFYG6j"
+},
+"AC1003": {
+    password: "1003",
+    name: "Md Nahiyan Al Noman",
+    reg: "NON GOVT ID",
+    mobile: "01550684739",
+    email: "nahiyan4739@gmail.com",
+    address: "Hatibandha, Lalmonirhat",
+    img: "https://lh3.googleusercontent.com/d/1ELDgbSTZlM1HCVVQ8JfKtuBLpK1nIxUg"
+},
+"AC1002": {
+    password: "1002",
+    name: "Indro jeet Chakrabortty",
+    reg: "NON GOVT ID",
+    mobile: "01978860435",
+    email: "pemborntakp@gmail.com",
+    address: "Jhenaidah,kaligonj",
+    img: "https://lh3.googleusercontent.com/d/1hNjU8fQvhWYIam5kYE6sNNU4Vl6jL273"
+},
+"AC1001": {
+    password: "1001",
+    name: "Jotty Sharma",
+    reg: "NON GOVT ID",
+    mobile: "01772048895",
+    email: "jottysharma887@gmail.com",
+    address: "Jhenaidah",
+    img: "https://lh3.googleusercontent.com/d/13bRVk7iEzqgyu6e-dUDiWCRqEMOWItTH"
+},
+"AC1000": {
+    password: "1000",
+    name: "borshon kundu",
+    reg: "NON GOVT ID",
+    mobile: "01323764547",
+    email: "borshonkundu499@gmail.com",
+    address: "Fulhari, Jhenaidah",
+    img: "https://lh3.googleusercontent.com/d/1lBrNobdfAJoggT2fmSR9bmOvnBeJrELH"
+}
 };
